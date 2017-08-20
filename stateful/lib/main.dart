e75fe0b83d944fd31c30717b05c4834cb69bf4f2
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-08-20 17:55:28 -0500

    State, Buttons

--- stateful/lib/main.dart
+++ stateful/lib/main.dart
@@ -3,17 +3,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new MaterialApp(
-      home: new Button()
+      home: new MaterialButton()
     )
   );
 }
 
-class Button extends StatefulWidget {
+class MaterialButton extends StatefulWidget {
   @override
   ButtonState createState() => new ButtonState();
 }
 
-class ButtonState extends State<Button> {
+class ButtonState extends State<MaterialButton> {
+
+  int counter = 0;
+  List<String> mStrings = ["Flutter", "is", "Awesome"];
+  String mDisplayMessage = " ";
+
+  void onBtnPressed() {
+   setState((){
+     mDisplayMessage = mStrings[counter];
+     counter = counter < mStrings.length - 1 ? counter + 1 : 0;
+   });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -22,6 +34,21 @@ class ButtonState extends State<Button> {
         backgroundColor: Colors.amberAccent,
 
       ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(mDisplayMessage),
+              new RaisedButton(
+                child: new Text("Show Data"),
+                color: Colors.blueAccent,
+                onPressed: onBtnPressed
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
