2379a58227b3a74f34b86eed2d3da794e58ce912
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-08-21 13:02:10 -0500

    UI components init

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -14,13 +14,40 @@ class ChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Chat App"),
       ),
+      body: _buildTextComposer(),
     );
   }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(
+            hintText: "Send a message"
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted (String messageText) {
+
+  }
+
+
 }
